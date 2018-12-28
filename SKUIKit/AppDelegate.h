3fa3861f34ec0db1b25b6d3d6296efd81fef3438
sk <lylapp@163.com>
2018-12-28 13:10:17 +0800

    Initial Commit

---
+++ SKUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKUIKit
+//
+//  Created by sk on 2018/12/28.
+//  Copyright © 2018 sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKUIKit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SKUIKit
+//
+//  Created by sk on 2018/12/28.
+//  Copyright © 2018 sk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
